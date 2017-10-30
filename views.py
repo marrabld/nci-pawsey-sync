2f71a0a334f7e82291cc717ec3f9afea3d376f91
Dan Marrable <marrabld@gmail.com>
2017-10-30 15:09:24 +0800

    Added a few more files that are required.

--- views.py
+++ views.py
@@ -1,7 +1,8 @@
 from flask import render_template, jsonify, request
 from app import app
-from hardware_utils import linux_hardware
-from weather_utils import meteye
+#from hardware_utils import linux_hardware
+#from weather_utils import meteye
+from api import nci, pawsey
 
 
 @app.route('/')
@@ -9,31 +10,31 @@ def hello_world():
     return render_template('index.html')
 
 
-@app.route('/blk_devices')
-def get_blk_devices():
-    response = linux_hardware.get_blk_devies()
-    return render_template('hardware.html', response=response)
-
-
-@app.route('/hardware')
-def get_hardware():
-    response = linux_hardware.get_hardware()
-    return response
-
-
-@app.route('/api/v0/dev/<string:device>')
-def get_disk(device):
-    response = linux_hardware.get_device_size(device)
-
-    if len(response) == 0:
-        return "<strong>No such device \"{}\" </strong>".format(device)
-    else:
-        return jsonify(response)
-
-
-@app.route('/api/v1/weather', methods=['GET'])
-def get_weather():
-    location = request.args['location']
-
-    response = meteye.get_weather(location, time=None)
-    return jsonify(response)
+# @app.route('/blk_devices')
+# def get_blk_devices():
+#     response = linux_hardware.get_blk_devies()
+#     return render_template('hardware.html', response=response)
+#
+#
+# @app.route('/hardware')
+# def get_hardware():
+#     response = linux_hardware.get_hardware()
+#     return response
+#
+#
+# @app.route('/api/v0/dev/<string:device>')
+# def get_disk(device):
+#     response = linux_hardware.get_device_size(device)
+#
+#     if len(response) == 0:
+#         return "<strong>No such device \"{}\" </strong>".format(device)
+#     else:
+#         return jsonify(response)
+#
+#
+# @app.route('/api/v1/weather', methods=['GET'])
+# def get_weather():
+#     location = request.args['location']
+#
+#     response = meteye.get_weather(location, time=None)
+#     return jsonify(response)
